8e54472b59d2b6225ebc7a57566f026ce39335ee
ssaeidd <saeid.basirnia@gmail.com>
2018-01-16 20:52:27 +0330

    Initial Commit

---
+++ SwiftyOverlay/SwiftyOverlay.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyOverlay.h
+//  SwiftyOverlay
+//
+//  Created by Saeid Basirnia on 1/16/18.
+//  Copyright © 2018 Saeid Basirnia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyOverlay.
+FOUNDATION_EXPORT double SwiftyOverlayVersionNumber;
+
+//! Project version string for SwiftyOverlay.
+FOUNDATION_EXPORT const unsigned char SwiftyOverlayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyOverlay/PublicHeader.h>
+
+

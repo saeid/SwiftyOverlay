aec758b228e936089cc51aa9794816aef5be6cc9
Saeid <basirnia@icloud.com>
2021-08-04 19:50:35 +0200

    Update Package file

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,7 +13,7 @@ let package = Package(
     dependencies: [],
     targets: [
         .target(
-            name: "SwiftyOverlay",
+            name: "Sources/SwiftyOverlay",
             dependencies: []),
         .testTarget(
             name: "SwiftyOverlayTests",

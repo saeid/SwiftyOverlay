d1daf23aaee800b42bc32304e80b9fe97eda1403
Saeid <basirnia@icloud.com>
2021-08-04 19:03:31 +0200

    Add SPM support

---
+++ Tests/SwiftyOverlayTests/SwiftyOverlayTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import SwiftyOverlay
+
+    final class SwiftyOverlayTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(SwiftyOverlay().text, "Hello, World!")
+        }
+    }
